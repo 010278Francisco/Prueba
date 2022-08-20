6590cf14a4b3b1a95f1c0415283d63d883f888d6
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-19 22:56:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "prueba" {
   ami           = "ami-090fa75af13c156b4"
   instance_type = "t2.micro"
-  security_groups= ["launch-wizard-1","default"]
+  security_groups= ["launch-wizard-3","default"]
   key_name= "name"
   
 
