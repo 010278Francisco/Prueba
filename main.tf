e0bf7bb8fa136086066da8ae4b69aad797423157
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-19 20:44:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAQ2C4ISCQYA6CBIN6"
-  secret_key = "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"
 }
 
 resource "aws_instance" "prueba" {
