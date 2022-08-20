903cd9bf51560785f6dee0de56f8f09cc1f2ed48
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-19 19:58:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
   access_key = "AKIAQ2C4ISCQYA6CBIN6"
   secret_key = "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"
 }
